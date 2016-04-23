4cb0d51b7f8e3adc7c070dd42f371abc70c40d11
Wojtek <wm360333@students.mimuw.edu.pl>
2016-04-23 18:53:45 +0200

    Minor style changes.

---
+++ config.py
@@ -0,0 +1,19 @@
+"""
+    Configuration file.
+"""
+
+# spectogram
+DATA_POINTS = 4096
+NUM_OVER = 2048
+FREQ = 10000
+
+# get2DPeaks
+AMP_MIN = 10
+PEAK_NEIGHBORHOOD_SIZE = 20
+
+# hashes
+NUM_OF_PEAKS = 15 # number of neighborhood peaks to consider
+MAX_TIME_DIFF = 150 # maximum time difference
+
+# recognize
+THRESHOLD = 30
---
+++ dbConfig.py
@@ -0,0 +1,10 @@
+"""
+    Configuration file for database connection.
+"""
+
+# TODO: create separate user with read-only rights for recognition.
+
+DBNAME = 'music'
+HOST = 'localhost'
+USER = 'wojtek'
+PASSWD = 'test'
